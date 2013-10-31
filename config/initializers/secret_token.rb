aad07bb24dabe71055d86b9c977eb95a4d2cffaf
Leon Hao <haolulue@163.com>
2013-10-31 11:14:34 +0800

    add doccenter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doccenter::Application.config.secret_key_base = '614f17da49425565e23e71cd3e364258dca937ecab10a3965a9e48e99ac4de51ee07cfd51e46b0b0c57a54283fc8cafde6e807da528322d7f24769f6d5ff4f56'
